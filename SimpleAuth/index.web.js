d09eeec38e41e1c5adc886fc889fe63d04b1aadd
Otto Grajeda <ottograjeda@gmail.com>
2019-08-16 13:17:23 -0700

    Update

---
+++ SimpleAuth/index.web.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from './app/components/App'; // new = part of monorepo
+//import App from './app/web/'; // old = uses old folder structure where 'web' is not part of monorepo
+
+// Render is @ App = not needed here
+//ReactDOM.render(<App/>, document.getElementById('root')); // is loaded @ app/web/index.js
+
+console.log('o.log > index.web.js called')
