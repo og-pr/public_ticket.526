1bc3300824345efde7b4ff64ec115e7174f5f78f
Otto Grajeda <ottograjeda@gmail.com>
2019-08-22 11:44:14 -0700

    Update

--- SimpleAuth/index.ios.js
+++ SimpleAuth/index.ios.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 
-//console.disableYellowBox = true; // disable warning regarding componentWillMount is deprecated and will be removed in the next major version
+console.disableYellowBox = true; // disable warning regarding componentWillMount is deprecated and will be removed in the next major version
 
 import App from './app/components/App'; // new = part of monorepo
 AppRegistry.registerComponent('SimpleAuth', () => App);
